4586e91303bc4d23d18924aa1639535bc23a0913
Todd Thomas <toddthomas@acm.org>
2020-07-03 23:17:51 -0600

    Initial Commit

---
+++ mandelplot0/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mandelplot0
+//
+//  Created by Todd Thomas on 7/3/20.
+//  Copyright © 2020 Todd Thomas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
